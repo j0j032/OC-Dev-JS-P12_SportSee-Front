0ffd5570d46631af5540548e124df5a9df8e5139
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-25 19:46:09 +0200

    ✨ Perf chart is done

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
-
 import Loader from './Loader'
+
+const CustomTicks = ({payload, x, y, cx, cy, ...rest}) => {
+	return (
+		<text {...rest} y={y + (y-cy)/8} x={x+ (x- cx)/82}>
+			{payload.value}
+		</text>
+	)
+}
+
 
 const Performance = ({perfData}) => {
 	const {data, error, isLoading} = perfData
-	const {kind} = data
-	const formatData = () => data.data.map((item, index)=>({name:kind[index+1], value: item.value}))
+	
+//	const {kind} = data
+//	const formatData = () => data.data.map((item, index)=>({name:kind[index+1], value: item.value}))
+	
+	const kindInFrench = ['Cardio', 'Energie', 'Endurance', 'Force', 'Vitesse', 'Intensité']
+	const formatData = () => data.data.map((item, index)=>({name:kindInFrench[index], value: item.value}))
 	console.log(formatData())
 	
 	if (error) return <span>Oups ! il y a eu un problème</span>
 	return isLoading ? (<Loader/>) : (
 		<div className='perf-container'>
 			<ResponsiveContainer width="100%" height="100%">
-				<RadarChart cx="50%" cy="50%" outerRadius="80%" data={formatData()}>
-					<PolarGrid />
-					<PolarAngleAxis dataKey="name" />
-					<Radar name="Mike" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+				<RadarChart data={formatData().reverse()}
+							cx="50%" cy="50%"
+							outerRadius="70%">
+					<PolarGrid radialLines={false}/>
+					<PolarAngleAxis dataKey="name"
+									stroke='#fffefc'
+									tick={<CustomTicks cx={320} cy={90}/>}
+									tickLine={false}
+									dy={0}
+					/>
+					<Radar dataKey="value"
+						   fill="red"
+						   fillOpacity={0.7}
+					/>
 				</RadarChart>
 			</ResponsiveContainer>
 		</div>
