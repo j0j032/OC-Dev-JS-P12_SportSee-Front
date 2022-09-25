61cf3ff5e6ae94232506decc4c66ad6d9b79a01b
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-25 15:56:10 +0200

    🚧 Indent improvement

--- src/components/useGetDatas.js
+++ src/components/useGetDatas.js
@@ -13,16 +13,11 @@ export const useGet = (endPoint) => {
 		setLoading(true)
 		axios
 			.get(userRoute+endPoint)
-			.then((response) => setData(response.data.data)
-			
-			)
-			
-			.catch((error) => {
-				console.log(error)
+			.then((response) => setData(response.data.data))
+			.catch((error) => {console.log(error)
 				setError(error)
 			})
 			.finally(() => setLoading(false))
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 	
 	return { isLoading, data, error }
