2d80807031e8d1fabe53a44f970c7f668831db86
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-27 19:35:08 +0200

    📝 Add JS-doc and propTypes to Performance.jsx

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 import Loader from './Loader'
+import PropTypes, {object} from 'prop-types'
+import Activity from './Activity'
 
+/**
+ * To add padding between the chart and labels
+ * @param {array} payload
+ * @param {number} x
+ * @param {number} y
+ * @param {number} cx
+ * @param {number} cy
+ * @param {object} rest
+ * @returns {JSX.Element}
+ * [StackOverflow Ressource]{@link https://stackoverflow.com/questions/65447592/recharts-is-there-a-way-to-add-a-padding-between-the-chart-and-labels-for-rada }
+ */
 const CustomTicks = ({payload, x, y, cx, cy, ...rest}) => {
 	return (
 		<text {...rest} y={y + (y-cy)/8} x={x+ (x- cx)/82}>
@@ -10,14 +23,24 @@ const CustomTicks = ({payload, x, y, cx, cy, ...rest}) => {
 	)
 }
 
-
+/**
+ * Component to display the Performance chart using Recharts library
+ * @param {object} perfData
+ * @returns {JSX.Element}
+ * [Recharts RadarChart doc]{@link  https://recharts.org/en-US/api/RadarChart}
+ */
 const Performance = ({perfData}) => {
 	const {data, error, isLoading} = perfData
-	
+	const kindInFrench = ['Cardio', 'Energie', 'Endurance', 'Force', 'Vitesse', 'Intensité']
+
+//  To get original data in english
 //	const {kind} = data
 //	const formatData = () => data.data.map((item, index)=>({name:kind[index+1], value: item.value}))
 	
-	const kindInFrench = ['Cardio', 'Energie', 'Endurance', 'Force', 'Vitesse', 'Intensité']
+	/**
+	 * To translate the labels in French using kindInFrenchArray
+	 * @returns {array} of {{object} {name: {string}, value: {number}}}
+	 */
 	const formatData = () => data.data.map((item, index)=>({name:kindInFrench[index], value: item.value}))
 	
 	if (error) return <span>Oups ! il y a eu un problème</span>
@@ -46,3 +69,15 @@ const Performance = ({perfData}) => {
 }
 
 export default Performance
+
+Activity.propTypes = {
+	perfData: PropTypes.object,
+	isLoading: PropTypes.bool,
+	error:PropTypes.bool,
+	data:PropTypes.object,
+	kindInFrench:PropTypes.array,
+	formatData:PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string, //After formating (before formating Proptype = string (date))
+		value: PropTypes.number,
+	}))
+}
