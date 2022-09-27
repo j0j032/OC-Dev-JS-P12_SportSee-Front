d06868839cfd320e8f021fdcc7699a182e451365
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-27 20:08:09 +0200

    📝 Add JS-doc and propTypes to KeyInfo.jsx

--- src/components/KeyInfo.jsx
+++ src/components/KeyInfo.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
+/**
+ * Component to display a user key info
+ * @param {number} keyData
+ * @param {string} img
+ * @param {string} unit
+ * @param {string} category
+ * @returns {JSX.Element}
+ */
 const KeyInfo = ({keyData, img, unit,category }) => {
 	return (
 		<div>
@@ -15,3 +24,10 @@ const KeyInfo = ({keyData, img, unit,category }) => {
 }
 
 export default KeyInfo
+
+KeyInfo.propTypes = {
+	keyData: PropTypes.number,
+	img: PropTypes.string,
+	unit: PropTypes.string,
+	category: PropTypes.string,
+}
