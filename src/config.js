0bfcac981367e341eb0b6d494792016ceb2f8458
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-27 17:42:21 +0200

    📝 Add JS-doc and propTypes to config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,12 @@
+import PropTypes from 'prop-types'
+
+/**
+ * DEVELOPPEMENT ENV ONLY
+ * This method set a user in terms of a localHost port in terms of the mock api or the micro
+ * provisional api using cross-env.
+ * So you can work on both backend and have a different user for each env.
+ * @returns {{userId: {string}, url: {string}}}
+ */
 const provisionalConfig = () => {
 	const setPort = process.env.REACT_APP_ENVIRONMENT === 'mockedApi' ? ['3001','18'] : ['3000','12']
 	return {
@@ -8,3 +17,7 @@ const provisionalConfig = () => {
 
 export default provisionalConfig
 
+provisionalConfig.propTypes = {
+	url: PropTypes.string,
+	userId : PropTypes.string
+}
